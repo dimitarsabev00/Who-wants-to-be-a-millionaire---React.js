7e3ac539a2e5a03e4703306350d84a003ec1fe7e
Dimitar <wopz1337@gmail.com>
2022-10-15 12:58:35 +0300

    Added Sound In Application

--- src/components/BoxQuestion.jsx
+++ src/components/BoxQuestion.jsx
@@ -1,9 +1,20 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./BoxQuestion.css";
+import useSound from "use-sound";
+import play from "../assets/play.mp3";
+import correct from "../assets/correct.mp3";
+import wrong from "../assets/wrong.mp3";
 const BoxQuestion = ({ data, setStop, questionNumber, setQuestionNumber }) => {
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState("answer");
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
+  useEffect(() => {
+    letsPlay();
+  }, [letsPlay]);
 
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
@@ -21,12 +32,19 @@ const BoxQuestion = ({ data, setStop, questionNumber, setQuestionNumber }) => {
     delay(3000, () => {
       setClassName(answer.correct ? "answer correct" : "answer wrong");
     });
-    delay(6000, () => {
+    delay(5000, () => {
       if (answer.correct) {
-        setQuestionNumber((prev) => prev + 1);
-        setSelectedAnswer(null);
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber((prev) => prev + 1);
+          setSelectedAnswer(null);
+        });
       } else {
-        setStop(true);
+        wrongAnswer();
+
+        delay(1000, () => {
+          setStop(true);
+        });
       }
     });
   };
